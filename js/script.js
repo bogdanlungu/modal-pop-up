16b3feccd0195d494de4a347a9325679282764bf
Bogdan Lungu <lungu.c.bogdan@gmail.com>
2016-03-26 21:48:09 +0200

    Added drag and drop for the modal window

--- js/script.js
+++ js/script.js
@@ -9,6 +9,36 @@ $(function(){
     $('.close').click(function(){
         $('.modal, .overlay').fadeOut('fast');
     });
+
+    $('.drag').on("mousedown", function(){
+        $(this).addClass('dragging');
+    }).on("mousemove mouseout", function(event){
+        if ($(this).hasClass("dragging")) {
+
+            var left = (event.pageX - ($(this).width() / 2));
+            if (left > ($(window).width() - $(this).width())) {
+                left = ($(window).width() - $(this).width());
+            } else if(left <= 0) {
+                left = 0;
+            }
+
+            var top = (event.pageY - ($(this).height() / 2));
+            if (top > ($(window).height() - $(this).height())) {
+                top = ($(window).height() - $(this).height());
+            } else if (top <= 0) {
+                top = 0;
+            }
+
+            $(this).css({
+                top: top + "px",
+                left: left + "px",
+                position: "absolute"
+            });
+        }
+    }).on("mouseup", function(){
+        $(this).removeClass('dragging');
+    });
+
 });
 function modalPosition() {
     var width = $('.modal').width();
